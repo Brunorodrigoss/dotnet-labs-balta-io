f8ca4cd55efb1189a271a23efeee0eb6c5759193
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-06-02 09:17:04 +0100

    Using Guid

---
+++ MyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingGuid();
+        }
+
+        static void UsingGuid()
+        {
+            var id = Guid.NewGuid();
+            Console.WriteLine(id);
+        }
+    }
+
+
+}
